38f31b43948379d620b81367dbebe59f253ea02e
Corey Giles <corey.giles@curtin.edu.au>
2017-01-16 15:05:25 +0800

    Update Fluxomics.R

--- Fluxomics.R
+++ Fluxomics.R
@@ -1,1 +1,9 @@
 ####    Fluxomics R file
+
+##  Application of stable isotopes to investigate the metabolism of fatty acids, glycerophospholipid and sphingolipid species
+#   http://www.sciencedirect.com/science/article/pii/S0163782714000034
+#   Review, showing strategies to track and measure lipid metabolism
+
+##  Determining the Turnover of Glycosphingolipid Species by Stable-Isotope Tracer Lipidomics
+#   http://www.sciencedirect.com/science/article/pii/S0022283616302212
+#   Use of 13C glucose to study glycosphingolipids and 13C serine for de novo synthesis of sphingolipids
